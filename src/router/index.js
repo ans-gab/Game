3ad7ee3616331bda3fb7e327ba38d323e5dc6e41
superson <474015445@qq.com>
2023-06-08 17:17:17 +0800

    新增猜姓氏页面路由跳转配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,27 @@ import React from "react";
 import { Navigate } from "react-router";
 import Home from "../pages/Home";
 import Mindreading from "../pages/Game/Mindreading";
+import Guessingsurnames from "../pages/Game/Guessingsurnames";
 
 
-const routes=[
+const routes = [
     {
-        path:'/',
-        element:<Navigate to = '/home'/>
+        path: '/',
+        element: <Navigate to='/home' />
     },
     {
-        path:'home',
-        element:<Home/>
+        path: 'home',
+        element: <Home />
     },
     {
-        path:'mindreading',
-        element:<Mindreading/>
+        path: 'mindreading',
+        element: <Mindreading />
     },
+    {
+        path: 'guessingsurnames',
+        element: <Guessingsurnames />
+
+    }
 ]
 
 export default routes
